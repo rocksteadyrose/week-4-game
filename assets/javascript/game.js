078155e13f3756868c77faef18c402cd82c56c58
Leslie Rose <leslierose@Leslies-MBP.attlocal.net>
2018-03-16 23:18:52 -0500

    changes to characters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -23,76 +23,101 @@
 
 //VARIABLES
 
+var userChoice = false;
+var enemyChoice = false;
+var startGame = false;
+var enemy;
+var char;
+var chosenCharacter;
 
-var characterChosen = "";
-var enemyChosen = [];
-
-var character = [
-        obiwan = 
+var character = {
+       obiwan: 
         {name: "Obi-Wan Kenobi", 
         image: "<img class='characterclass' src='assets/images/obiwankenobi.jpg' width='150px' height='100px'>",
         health: 120},
 
-        luke =
+        luke:
         {name: "Luke Skywalker",
         image: "<img class='characterclass' src='assets/images/lukeskywalker.jpg' width='150px' height='100px'>",
         health: 100},
 
-        darthsidious =
+       darthsidious:
         {name: "Darth Sidious",
         image: "<img class='characterclass' src='assets/images/darthsidious.jpg' width='150px' height='100px'>",
         health: 150},
 
-        darthmaul =
+        darthmaul:
         {name: "Darth Maul",
         image: "<img class='characterclass' src='assets/images/darthmaul.jpg' width='150px' height='100px'>",
-        health: 180},
-];
+        health: 180}
+};
 
 var obiwanimage = $(".characters");
-obiwanimage.append("<div class = 'thumbnail'>"+obiwan.name+"<br>"+obiwan.image+"<br><h4>Health: "+obiwan.health + "</h4></div>");
+obiwanimage.append("<div class = 'thumbnail' id='obiwan'>"+character.obiwan.name+"<br>"+character.obiwan.image+"<br><h4>Health: "+character.obiwan.health + "</h4></div>");
 var lukeimage = $(".characters");
-lukeimage.append("<div class = 'thumbnail'>"+luke.name+"<br>"+luke.image+"<br><h4>Health: "+luke.health+"</h4></div>");
+lukeimage.append("<div class = 'thumbnail' id='luke'>"+character.luke.name+"<br>"+character.luke.image+"<br><h4>Health: "+character.luke.health+"</h4></div>");
 var darthsidiousimage = $(".characters");
-darthsidiousimage.append("<div class = 'thumbnail'>"+darthsidious.name+"<br>"+darthsidious.image+"<br><h4>Health: "+darthsidious.health+"</h4></div>");
+darthsidiousimage.append("<div class = 'thumbnail' id='darthsidious'>"+character.darthsidious.name+"<br>"+character.darthsidious.image+"<br><h4>Health: "+character.darthsidious.health+"</h4></div>");
 var darthmaulimage = $(".characters");
-darthmaulimage.append("<div class = 'thumbnail'>"+darthmaul.name+"<br>"+darthmaul.image+"<br><h4>Health: "+darthmaul.health+"</h4></div>");
+darthmaulimage.append("<div class = 'thumbnail' id='darthmaul'>"+character.darthmaul.name+"<br>"+character.darthmaul.image+"<br><h4>Health: "+character.darthmaul.health+"</h4></div>");
 
 $('.characters').append(obiwanimage);
 $('.characters').append(lukeimage);
 $('.characters').append(darthsidiousimage);
 $('.characters').append(darthmaulimage);
 
-//START GAME
 
-//function startGame() {
-    //chooseCharacter(character);
-   // clickCharactersInitial();
-   // }
-
-//CHOOSE YOUR CHARACTER
-
-
+  // CLICK FOR INITIAL CHARACTER
+   $(".thumbnail").on( "click", function() {
+            chosenCharacter = $(this).data("chosenCharacter");
+            $('.yourcharacter').append($(this));
+            $(".yourcharacter").attr("class", "yourcharacter");
+            $(".characters").attr("class", "enemies");
+            $('.enemiesavailable').append($(".enemies"));
+            }
+          
+        )
         
-            $(".col-lg-12 .thumbnail").on( "click", function() {
-                for (var i = 0; i < character.length; i++) {
+    
+        
 
-                if (character[i] != this) {
-                $('.enemiesavailable').append("<div class = 'thumbnail'>"+character[i].name+"<br>"+character[i].image+"<br><h4>Health: "+character[i].health+"</h4></div>"); 
-                }
+  // CHOOSE CHARACTER
+    function chooseCharacter(character) {
+    characterChosen = true;
+     }
 
-                if (character[i] = this) {
-                    $('.yourcharacter').append(character[i]);
-                    $('.characters').empty();
-                    $(character[i]).remove('.enemiesavailable');}
+     // CHOOSE ENEMY
+    function chooseEnemy(character) {
+    character = enemy;  
+    //enemy.push(character);
+}
+    
 
-                    $(".thumbnail").on( "click", function() {
-                            if (character[i] = this) {
-                                $('.defendersection').append(character[i]);}
-                            })
-                }})
 
-                           
+
+//  $(char)).remove('.enemiesavailable');
+
+     //CHOOSE ENEMY
+  //     $(".thumbnail").on( "click", function() {
+   //     for (var i = 0; i < character.length; i++) {
+    //        if (character[i] = this) {
+    //           $('.defendersection').append(character[i]);
+     //            $(character[i]).addClass("defender-character").removeClass("enemy-character");
+     //            ;}
+    //         })
+
+    //     if (character[i] != this) {
+    //         $('.enemiesavailable').append("<div class = 'thumbnail'>"+character[i].name+"<br>"+character[i].image+"<br><h4>Health: "+character[i].health+"</h4></div>");
+  //           startGame = true;
+   //            $('.enemiesavailable').append(enemyChosen);
+// }
+//Add in disabling of buttons above
+   // START GAME
+
+   // $(".attackButton").on( "click", function() {
+   //  $('.defendersection').append("<b>You attacked</b>" + defender + "for" + defender.health + "damage." + defender + "attacked you back for" + defender.health + "damage.");
+//})
+
                 
 
             
