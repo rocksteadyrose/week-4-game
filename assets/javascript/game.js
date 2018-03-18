8766e8479c6d27402203fb1135b2ea4c977dd202
Leslie Rose <leslierose@Leslies-MBP.attlocal.net>
2018-03-18 02:50:09 -0500

    function changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -28,40 +28,45 @@ var characterChosen = false;
 var yourCharacter;
 var enemyChosen = false;
 var startGame = false;
+var defenderChosen = false;
 var enemy;
-var char;
 var defender;
 
-var character = [
-       obiwan= 
+var character = {
+       obiwan: 
         {name: "Obi-Wan Kenobi", 
         image: "<img class='characterclass' src='assets/images/obiwankenobi.jpg' width='150px' height='100px'>",
-        health: 120},
+        health: 120,
+        damage: 8
+    },
 
-        luke=
+        luke:
         {name: "Luke Skywalker",
         image: "<img class='characterclass' src='assets/images/lukeskywalker.jpg' width='150px' height='100px'>",
-        health: 100},
+        health: 100,
+        damage: 0},
 
-       darthsidious=
+       darthsidious:
         {name: "Darth Sidious",
         image: "<img class='characterclass' src='assets/images/darthsidious.jpg' width='150px' height='100px'>",
-        health: 150},
+        health: 150,
+        damage: 0},
 
-        darthmaul=
+        darthmaul:
         {name: "Darth Maul",
         image: "<img class='characterclass' src='assets/images/darthmaul.jpg' width='150px' height='100px'>",
-        health: 180}
-];
+        health: 180,
+        damage: 25,}
+};
 
 var obiwanimage = $(".characters");
-obiwanimage.append("<div class = 'thumbnail' id='obiwan'>"+obiwan.name+"<br>"+obiwan.image+"<br><h4>Health: "+obiwan.health + "</h4></div>");
+obiwanimage.append("<div class = 'thumbnail' id='obiwan'>"+character.obiwan.name+"<br>"+character.obiwan.image+"<br><h4>Health: "+character.obiwan.health + "</h4></div>");
 var lukeimage = $(".characters");
-lukeimage.append("<div class = 'thumbnail' id='luke'>"+luke.name+"<br>"+luke.image+"<br><h4>Health: "+luke.health+"</h4></div>");
+lukeimage.append("<div class = 'thumbnail' id='luke'>"+character.luke.name+"<br>"+character.luke.image+"<br><h4>Health: "+character.luke.health+"</h4></div>");
 var darthsidiousimage = $(".characters");
-darthsidiousimage.append("<div class = 'thumbnail' id='darthsidious'>"+darthsidious.name+"<br>"+darthsidious.image+"<br><h4>Health: "+darthsidious.health+"</h4></div>");
+darthsidiousimage.append("<div class = 'thumbnail' id='darthsidious'>"+character.darthsidious.name+"<br>"+character.darthsidious.image+"<br><h4>Health: "+character.darthsidious.health+"</h4></div>");
 var darthmaulimage = $(".characters");
-darthmaulimage.append("<div class = 'thumbnail' id='darthmaul'>"+darthmaul.name+"<br>"+darthmaul.image+"<br><h4>Health: "+darthmaul.health+"</h4></div>");
+darthmaulimage.append("<div class = 'thumbnail' id='darthmaul'>"+character.darthmaul.name+"<br>"+character.darthmaul.image+"<br><h4>Health: "+character.darthmaul.health+"</h4></div>");
 
 $('.characters').append(obiwanimage);
 $('.characters').append(lukeimage);
@@ -70,48 +75,119 @@ $('.characters').append(darthmaulimage);
 
 
   // CLICK FOR INITIAL CHARACTER
-   $(".thumbnail").on( "click", function() {
-       if (characterChosen === false && enemyChosen === false) {
-        
-            $('.yourcharacter').append($(this));
-            $(".yourcharacter").attr("class", "yourcharacter");
-            characterChosen === true;
-            char = $(this);
-            chooseCharacter(char);
-            $(".characters").attr("class", "enemies"); //Move remaining characters by giving new attribute and then moving them to enemies section
-            $('.enemiesavailable').append($(".enemies"));
-            enemyChosen === true;
-            $('.thumbnail').on( "click", function() {
-            defender = $(this).data("defender");
-            $('.defendersection').append($(this));
-            $(".defendersection").attr("class", "defender");
-        })}
-            }
-        )
-    
-   //    $(".attackButton").on( "click", function() {
-   //     $('.defendersection').append("<b>You attacked</b>" + defender + "for" + defender.health + "damage." + defender + "attacked you back for" + defender.health + "damage.");
+  
+  $(".thumbnail").on( "click", function() {
+    if (characterChosen === false && enemyChosen === false) {
+    $(".yourcharacter").append($(this));
+    $(".yourcharacter").attr("class", "yourcharacter");
+    $(".characters").attr("class", "enemies"); //Move remaining characters by giving new attribute and then moving them to enemies section
+    $('.enemiesavailable').append($(".enemies"))
+    char = $(this);
+    console.log(char);
+    chooseEnemy(char)
+}
 
+    else  {
+    //defender = $(this).data("defender");
+    $('.defendersection').append($(this));
+    char = $(this);
+    $(".defendersection").attr("class", "defender")
+    chooseDefender(char);}})
     
-
-     //   function defender(){
-    //        defender = "";
-    //    }
-        
 
   // CHOOSE CHARACTER
   function chooseCharacter(char) {
-  if (char = $("<div class = 'thumbnail' id='luke'>"+luke.name+"<br>"+luke.image+"<br><h4>Health: "+luke.health+"</h4></div>"))
-  {char = luke; console.log(char)}
+    char;
+    console.log(char);
+
+    if (char = $("<div class = 'thumbnail' id='luke'>"+luke.name+"<br>"+luke.image+"<br><h4>Health: "+luke.health+"</h4></div>"))
+    {char = character.luke;}
+ 
+    if (char = $("<div class = 'thumbnail' id='darthmaul'>"+character.darthmaul.name+"<br>"+character.darthmaul.image+"<br><h4>Health: "+character.darthmaul.health+"</h4></div>"))
+    {char = character.darthmaul;}
+
+    if (char = $("<div class = 'thumbnail' id='darthsidious'>"+character.darthsidious.name+"<br>"+character.darthsidious.image+"<br><h4>Health: "+character.darthsidious.health+"</h4></div>"))
+        {char = character.darthsidious;}
+   
+    if (char = $("<div class = 'thumbnail' id='obiwan'>"+character.obiwan.name+"<br>"+character.obiwan.image+"<br><h4>Health: "+character.obiwan.health + "</h4></div>"))
+        {char = character.obiwan;}
+
+    characterChosen = true;
  }
-//
-     // CHOOSE ENEMY
-    //function chooseEnemy(character) {
- //   character = enemy;  
-    //enemy.push(character);
-//}
+
+// CHOOSE ENEMY
+function chooseEnemy(char) {
+
+    if (char = $("<div class = 'thumbnail' id='luke'>"+luke.name+"<br>"+luke.image+"<br><h4>Health: "+luke.health+"</h4></div>"))
+    {char = enemy;
+    enemy = character.luke;
+}
+ 
+    if (char = $("<div class = 'thumbnail' id='darthmaul'>"+character.darthmaul.name+"<br>"+character.darthmaul.image+"<br><h4>Health: "+character.darthmaul.health+"</h4></div>"))
+    {char = enemy;
+    enemy = character.darthmaul;
+}
+
+    if (char = $("<div class = 'thumbnail' id='darthsidious'>"+character.darthsidious.name+"<br>"+character.darthsidious.image+"<br><h4>Health: "+character.darthsidious.health+"</h4></div>"))
+    {char = enemy;
+    enemy = character.darthsidious;
+}
+   
+    if (char = $("<div class = 'thumbnail' id='obiwan'>"+character.obiwan.name+"<br>"+character.obiwan.image+"<br><h4>Health: "+character.obiwan.health + "</h4></div>"))
+    {char = enemy;
+    enemy = character.obiwan;
+
+    enemyChosen = true;
+}
+}
+
+        
+//CHOOSE DEFENDER
+function chooseDefender(char) { 
+        console.log(char);
+        defender;
+        console.log(defender);  
+         
+
+        if (char = $("<div class = 'thumbnail' id='darthmaul'>"+character.darthmaul.name+"<br>"+character.darthmaul.image+"<br><h4>Health: "+character.darthmaul.health+"</h4></div>"))
+        {char = defender;
+        defender = character.darthmaul;
+       }
+
+        if (char = $("<div class = 'thumbnail' id='luke'>"+character.luke.name+"<br>"+character.luke.image+"<br><h4>Health: "+character.luke.health+"</h4></div>"))
+        {char = defender
+        defender = character.luke;
+        }
+
+        if (char = $("<div class = 'thumbnail' id='darthsidious'>"+character.darthsidious.name+"<br>"+character.darthsidious.image+"<br><h4>Health: "+character.darthsidious.health+"</h4></div>"))
+        {char = defender;
+        defender = character.darthsidious;
+        }
+
+        if (char = $("<div class = 'thumbnail' id='obiwan'>"+character.obiwan.name+"<br>"+character.obiwan.image+"<br><h4>Health: "+character.obiwan.health + "</h4></div>"))
+        {char = defender;
+        defender = character.obiwan;}
+    }
+
+    $(".attackButton").on( "click", function() {
+        console.log(defender);
+        $('.message').html("<b>You attacked </b>" + defender.name + " for " + defender.damage + " damage. " + defender.name + " attacked you back for " + defender.damage + " damage.");
+    })
+
+
+
     
 
+
+
+
+    
+
+        
+
+
+
+        
 
 
 //  $(char)).remove('.enemiesavailable');
